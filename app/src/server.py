28751aa9b4e1b3ae1993a1fff2bb91a910a25a28
Romil Bheda <17021364+romilbheda@users.noreply.github.com>
2021-11-09 19:54:39 +0530

    Update server.py

--- app/src/server.py
+++ app/src/server.py
@@ -3,7 +3,7 @@ server = Flask(__name__)
 
 @server.route("/")
 def hello():
-    return "<h1>Python Containerized App running!!!</h1>"
+   return "<h1>Python Containerized App running!!!</h1>"
 
 if __name__ == "__main__":
    server.run(host='0.0.0.0')
